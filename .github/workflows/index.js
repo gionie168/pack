7e290c93657dec0d55e9a5a495476f4dddc3083d
gionie168 <161115839+gionie168@users.noreply.github.com>
2024-04-11 15:48:46 +0800

    Update index.js

--- .github/workflows/index.js
+++ .github/workflows/index.js
@@ -1,1 +1,11 @@
 console.log("Hello, World!");
+$ npm init
+  ...
+  package name: @gionie168/pack
+  ...
+  test command: exit 0
+  ...
+  npm install
+git add index.js package.json package-lock.json
+git commit -m "initialize npm package"
+git push
